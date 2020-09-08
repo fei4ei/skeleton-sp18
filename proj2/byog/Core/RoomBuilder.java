fba8bbdda25ced2352e1984957219dd49213051a
FL <lifeifei.chem@gmail.com>
2020-09-07 22:23:29 -0500

    roombuilder v5 modulo

--- proj2/byog/Core/RoomBuilder.java
+++ proj2/byog/Core/RoomBuilder.java
@@ -3,25 +3,25 @@ package byog.Core;
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
+import byog.lab5.HexWorld;
 
 import java.util.Random;
 
 public class RoomBuilder {
     private static final int WIDTH = 60;
-    private static final int HEIGHT = 40;
+    private static final int HEIGHT = 38;
     private static final long SEED = 2873123;
     private static final Random RANDOM = new Random(SEED);
-    public static int[] currPos = {20,20};
-    public static int[] lastPos = {25,25};
-
+    public static int[] currPos = {20,36};
+    public static int[] lastPos = {20,20};
 
     /**
      *
      * @param tiles
      */
     public static void WallBuilder(TETile[][] tiles, int WIDTH, int HEIGHT) {
-        for (int x = 2; x < WIDTH-2; x++) {
-            for (int y = 2; y < HEIGHT-2; y++) {
+        for (int x = 1; x < WIDTH-1; x++) {
+            for (int y = 1; y < HEIGHT-1; y++) {
                 if (tiles[x][y] == Tileset.FLOOR) {
                     wallbuilder(tiles, x, y);
                 }
@@ -50,56 +50,28 @@ public class RoomBuilder {
      */
     public static void RoomBuilder(TETile[][] tiles, int sideWidth, int sideHeight, int[] pos) {
         // build the floor within the room and/or hallway
-        for (int y = 1; y < sideHeight-1; y++) {
-            for (int x = 1; x < sideWidth-1; x++) {
-                if (pos[0] + x < 0 || pos[0] + x >= WIDTH || pos[1] + y < 0 || pos[1] + y >= HEIGHT) {
-                    continue;
+        for (int y = 0; y < sideHeight; y++) {
+            for (int x = 0; x < sideWidth; x++) {
+                /**
+                if (pos[0] + x < 1 || pos[0] + x >= WIDTH-1 || pos[1] + y < 1 || pos[1] + y >= HEIGHT-1) {
+                    System.out.println("out of bounds");
+                    break;
                 }
-                tiles[pos[0] + x][pos[1] + y] = Tileset.FLOOR;
+                 */
+                tiles[(pos[0] + x) % (WIDTH - 2) + 1][(pos[1] + y) % (HEIGHT - 2) + 1] = Tileset.FLOOR;
             }
         }
-
-        /**
-        // build the wall surrounding the room and/or hallway.
-        // TODO: ensure x,y coordiates of walls are between 0-WIDTH for x and 0-HEIGHT for y
-        for (int x = 0; x < sideWidth; x++) {
-            if (tiles[pos[0] + x][pos[1]] == Tileset.FLOOR) {
-                continue;
-            }
-            tiles[pos[0] + x][pos[1]] = Tileset.WALL;
-        }
-        for (int x = 0; x < sideWidth; x++) {
-            if (tiles[pos[0] + x][pos[1] + sideHeight - 1] == Tileset.FLOOR) {
-                continue;
-            }
-            tiles[pos[0] + x][pos[1] + sideHeight - 1] = Tileset.WALL;
-        }
-
-        for (int y = 0; y < sideHeight; y++) {
-            if (tiles[pos[0]][pos[1] + y] == Tileset.FLOOR) {
-                continue;
-            }
-            tiles[pos[0]][pos[1] + y] = Tileset.WALL;
-        }
-
-        for (int y = 0; y < sideHeight; y++) {
-            if (tiles[pos[0] + sideWidth - 1][pos[1] + y] == Tileset.FLOOR) {
-                continue;
-            }
-            tiles[pos[0] + sideWidth - 1][pos[1] + y] = Tileset.WALL;
-        }
-         */
     }
 
     public static void RandomRooms(TETile[][] tiles) {
-        int sidewidth = RANDOM.nextInt(7)+3;
-        int sideheight = RANDOM.nextInt(7)+3;
+        int sidewidth = RANDOM.nextInt(8)+1;
+        int sideheight = RANDOM.nextInt(8)+1;
 
         RoomBuilder(tiles, sidewidth, sideheight, currPos);
     }
 
     // TODO: ensure x,y coordiates of walls are between 0-WIDTH for x and 0-HEIGHT for y
-    private static void RoomConnector(TETile[][] tiles, int[] lastPos, int[] Pos) {
+    public static void RoomConnector(TETile[][] tiles, int[] lastPos, int[] Pos) {
         if (lastPos.equals(Pos)) {
             return;
         }
@@ -111,62 +83,13 @@ public class RoomBuilder {
 
         // draw a horizontal floor, sandwiched by walls
         for (int x = 0; x < maxX - minX + 1; x++) {
-            tiles[minX + x + 1][Pos[1]] = Tileset.FLOOR;
+            tiles[(minX + x) % (WIDTH - 2) + 1][Pos[1] % (HEIGHT - 2) + 1] = Tileset.FLOOR;
         }
-
-        /**
-        if (tiles[minX][Pos[1]]!=Tileset.FLOOR) {
-            tiles[minX][Pos[1]] = Tileset.WALL;
-        }
-        if (tiles[minX + maxX - minX + 2][Pos[1]]!=Tileset.FLOOR) {
-            tiles[minX + maxX - minX + 2][Pos[1]] = Tileset.WALL;
-        }
-
-        for (int x = -1; x < maxX - minX + 2; x++) {
-            if (tiles[minX + x + 1][Pos[1] - 1] == Tileset.FLOOR) {
-                continue;
-            }
-            tiles[minX + x + 1][Pos[1] - 1] = Tileset.WALL;
-        }
-        for (int x = -1; x < maxX - minX + 2; x++) {
-            if (tiles[minX + x + 1][Pos[1] + 1] == Tileset.FLOOR) {
-                continue;
-            }
-            tiles[minX + x + 1][Pos[1] + 1] = Tileset.WALL;
-        }
-         */
-
 
         //draw a vertical floor, sandwiched by walls
         for (int y = 0; y < maxY - minY + 1; y++) {
-            if (tiles[lastPos[0] + 1][minY + y] == Tileset.FLOOR) {
-                continue;
-            }
-            tiles[lastPos[0] + 1][minY + y] = Tileset.FLOOR;
+            tiles[lastPos[0] % (WIDTH - 2) + 1][(minY + y) % (HEIGHT - 2) + 1] = Tileset.FLOOR;
         }
-
-        /**
-        if (tiles[lastPos[0] + 1][minY - 1] != Tileset.FLOOR) {
-            tiles[lastPos[0] + 1][minY - 1] = Tileset.WALL;
-        }
-        if (tiles[lastPos[0] + 1][minY + maxY - minY + 1] != Tileset.FLOOR) {
-            tiles[lastPos[0] + 1][minY + maxY - minY + 1] = Tileset.WALL;
-        }
-
-        for (int y = -1; y < maxY - minY + 2; y++) {
-            if (tiles[lastPos[0]][minY + y] == Tileset.FLOOR) {
-                continue;
-            }
-            tiles[lastPos[0]][minY + y] = Tileset.WALL;
-        }
-
-        for (int y = -1; y < maxY - minY + 2; y++) {
-            if (tiles[lastPos[0] + 2][minY + y] == Tileset.FLOOR) {
-                continue;
-            }
-            tiles[lastPos[0] + 2][minY + y] = Tileset.WALL;
-        }
-         */
     }
 
     public static void main(String[] args) {
@@ -181,58 +104,15 @@ public class RoomBuilder {
             }
         }
 
-        // Testing Roomconnector and Roombuilder
-        // 20,20 to 25,25; 25,25 to 20,20; 15,25 to 25,15; 25,15 to 15,25.
-        /**
-        currPos[0] = 25;
-        currPos[1] = 15;
-        RoomBuilder(Tiles, 5, 5, currPos);
-        currPos[0] = 15;
-        currPos[1] = 25;
-        lastPos[0] = 25;
-        lastPos[1] = 15;
-        RoomConnector(Tiles, lastPos, currPos);
-        RoomBuilder(Tiles, 5, 5, currPos);
-        */
-
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 26; i++) {
             lastPos[0] = currPos[0];
             lastPos[1] = currPos[1];
-            currPos[0] = RANDOM.nextInt(55);
-            currPos[1] = RANDOM.nextInt(35);
+            currPos[0] = lastPos[0] + RANDOM.nextInt(5);
+            currPos[1] = lastPos[1] + RANDOM.nextInt(5);
             RandomRooms(Tiles);
             RoomConnector(Tiles, lastPos, currPos);
         }
 
-        // Testing RoomConnector: from {10,10} to {20, 20}, from {20, 20} to {10, 10},
-        // from {10, 20} to {20, 10}, and from {20, 10} to {10, 20}.
-
-        /**
-        int[] lastPos = {30,7};
-        int[] Pos = {7, 30};
-        RoomConnector(Tiles, lastPos, Pos);
-
-        //{3,40} will crash the program. Why?
-        int[] lastPos1 = {3,34};
-        int[] Pos1 = {34, 3};
-        RoomConnector(Tiles, lastPos1, Pos1);
-
-        int[] lastPos2 = {10,10};
-        int[] Pos2 = {30, 30};
-        RoomConnector(Tiles, lastPos2, Pos2);
-
-        int[] lastPos3 = {25,25};
-        int[] Pos3 = {15, 15};
-        RoomConnector(Tiles, lastPos3, Pos3);
-
-        int[] lastPos4 = {37,5};
-        int[] Pos4 = {37, 25};
-        RoomConnector(Tiles, lastPos4, Pos4);
-
-        int[] lastPos5 = {5,35};
-        int[] Pos5 = {30, 35};
-        RoomConnector(Tiles, lastPos5, Pos5);
-         */
         WallBuilder(Tiles, WIDTH, HEIGHT);
         ter.renderFrame(Tiles);
     }
--- proj2/byog/Core/TestRoomBuilder.java
+++ proj2/byog/Core/TestRoomBuilder.java
@@ -7,7 +7,8 @@ import static org.junit.Assert.*;
 
 public class TestRoomBuilder {
 
-    public static void main(String[] args) {
+    @Test
+    public static void TestWallBuilder(String[] args) {
         int WIDTH = 10;
         int HEIGHT = 10;
         TERenderer ter = new TERenderer();
@@ -21,10 +22,63 @@ public class TestRoomBuilder {
             }
         }
 
-        int[] pos = {3,3};
-        RoomBuilder.RoomBuilder(Tiles,5,5,pos);
+        int[] pos = {3, 3};
+        RoomBuilder.RoomBuilder(Tiles, 5, 5, pos);
         RoomBuilder.WallBuilder(Tiles, WIDTH, HEIGHT);
         ter.renderFrame(Tiles);
+    }
 
+    // Testing Roomconnector and Roombuilder
+    // 20,20 to 25,25; 25,25 to 20,20; 15,25 to 25,15; 25,15 to 15,25.
+
+    @Test
+    public static void TestRoomConnectorBuilder() {
+        int WIDTH = 10;
+        int HEIGHT = 10;
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] Tiles = new TETile[WIDTH][HEIGHT];
+
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                Tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        int[] currPos = {25,15};
+        int[] lastPos = {25,15};
+        RoomBuilder.RoomBuilder(Tiles, 5, 5, currPos);
+        currPos[0] = 15;
+        currPos[1] = 25;
+        RoomBuilder.RoomConnector(Tiles, lastPos, currPos);
+        RoomBuilder.RoomBuilder(Tiles, 5, 5, currPos);
+
+        /**
+         int[] lastPos = {30,7};
+         int[] Pos = {7, 30};
+         RoomConnector(Tiles, lastPos, Pos);
+
+         //{3,40} will crash the program. Why?
+         int[] lastPos1 = {3,34};
+         int[] Pos1 = {34, 3};
+         RoomConnector(Tiles, lastPos1, Pos1);
+
+         int[] lastPos2 = {10,10};
+         int[] Pos2 = {30, 30};
+         RoomConnector(Tiles, lastPos2, Pos2);
+
+         int[] lastPos3 = {25,25};
+         int[] Pos3 = {15, 15};
+         RoomConnector(Tiles, lastPos3, Pos3);
+
+         int[] lastPos4 = {37,5};
+         int[] Pos4 = {37, 25};
+         RoomConnector(Tiles, lastPos4, Pos4);
+
+         int[] lastPos5 = {5,35};
+         int[] Pos5 = {30, 35};
+         RoomConnector(Tiles, lastPos5, Pos5);
+         */
     }
 }
