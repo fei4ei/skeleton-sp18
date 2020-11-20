afda9dc3383a13bfa5402f9ce069de1da6dc08b5
FL <lifeifei.chem@gmail.com>
2020-11-20 09:34:11 -0600

    hw1 v1.1 comparator of search node written

--- hw4/hw4/puzzle/SearchNode.java
+++ hw4/hw4/puzzle/SearchNode.java
@@ -2,10 +2,12 @@ package hw4.puzzle;
 
 import edu.princeton.cs.algs4.MinPQ;
 
+import java.util.Comparator;
+
 public class SearchNode implements WorldState{
     WorldState ws;
-    int NumberOfMoves;
-    SearchNode Prev;
+    int NumberOfMoves; // he number of moves made to reach this world state from the initial state.
+    SearchNode Prev; // reference to the previous node
 
     public SearchNode(WorldState input) {
         ws = input;
@@ -19,18 +21,30 @@ public class SearchNode implements WorldState{
         Prev = previous;
     }
 
+    private static class SearchNodeComparator implements Comparator<SearchNode> {
+        @Override
+        public int compare(SearchNode o, SearchNode p) {
+            return (o.NumberOfMoves + o.ws.estimatedDistanceToGoal()) -
+                    (p.NumberOfMoves + p.ws.estimatedDistanceToGoal());
+        }
+    }
+
+    public static Comparator<SearchNode> SNComparator() {
+        return new SearchNodeComparator();
+    }
+
     @Override
     public int estimatedDistanceToGoal() {
-        return 0;
+        return ws.estimatedDistanceToGoal();
     }
 
     @Override
     public Iterable<WorldState> neighbors() {
-        return null;
+        return ws.neighbors();
     }
 
     public static MinPQ<SearchNode> creatPQ(WorldState inputstate) {
-        MinPQ<SearchNode> fringe = new MinPQ<>();
+        MinPQ<SearchNode> fringe = new MinPQ<>(SNComparator()); // SNComparator supplies the order in which to compare the keys
         SearchNode inputnode = new SearchNode(inputstate);
         fringe.insert(inputnode);
         return fringe;
