919e3b22fd86af271807178fa2ecde85bedca6fb
FL <lifeifei.chem@gmail.com>
2021-01-16 16:51:11 -0600

    countingsort v1.0

--- lab13/CountingSort.java
+++ lab13/CountingSort.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.TreeMap;
+
 /**
  * Class with 2 ways of doing Counting sort, one naive way and one "better" way
  *
@@ -30,7 +33,7 @@ public class CountingSort {
         // count number of times into the new array
         int[] sorted = new int[arr.length];
         int k = 0;
-        for (int i = 0; i < counts.length; i += 1) {
+        for (int i = 0; i < counts.length; i += 1) { // counts.length: alphabet size
             for (int j = 0; j < counts[i]; j += 1, k += 1) {
                 sorted[k] = i;
             }
@@ -66,7 +69,68 @@ public class CountingSort {
      * @param arr int array that will be sorted
      */
     public static int[] betterCountingSort(int[] arr) {
-        // TODO make counting sort work with arrays containing negative numbers.
-        return null;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i : arr) {
+            max = max > i ? max : i;
+            min = min < i ? min : i;
+        }
+
+        // gather all the counts for each value
+        int[] counts = new int[max - min + 1];
+        for (int i : arr) {
+            counts[i - min]++;
+        }
+
+        // when we're dealing with ints, we can just put each value
+        // count number of times into the new array
+        int[] sorted = new int[arr.length];
+        int k = 0;
+        for (int i = min; i < max + 1; i += 1) { // counts.length: alphabet size
+            for (int j = 0; j < counts[i - min]; j += 1, k += 1) {
+                sorted[k] = i;
+            }
+        }
+
+        // however, below is a more proper, generalized implementation of
+        // counting sort that uses start position calculation
+//        int[] starts = new int[max + 1];
+//        int pos = 0;
+//        for (int i = 0; i < starts.length; i += 1) {
+//            starts[i] = pos;
+//            pos += counts[i];
+//        }
+//
+//        int[] sorted2 = new int[arr.length];
+//        for (int i = 0; i < arr.length; i += 1) {
+//            int item = arr[i];
+//            int place = starts[item + min];
+//            sorted2[place] = item;
+//            starts[item + min] += 1;
+//        }
+
+        // return the sorted array
+        return sorted;
     }
+//        HashMap<Integer, Integer> counts = new HashMap();
+//        for (int i = 0; i < arr.length; i++) {
+//            if (!counts.containsKey(arr[i])) {
+//                counts.put(arr[i],1);
+//            } else {
+//                int count = counts.get(arr[i]);
+//                counts.put(arr[i], count+1);
+//            }
+//        }
+//
+//        TreeMap<Integer, Integer> starts = new TreeMap(); // TreeMap provides order, while HashMap does not
+//
+//        int[] sorted = new int[arr.length];
+//        for (int i = 0; i < arr.length; i++) {
+//            int index = starts.get(arr[i]);
+//            sorted[index] = arr[i];
+//            starts.put(arr[i], index + 1);
+//        }
+//        return sorted;
+//    }
+
 }
